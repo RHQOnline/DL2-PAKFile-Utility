1f38c1ef235681a3d6da0ae09d3ea38809160a8e
Rusty <42674949+RHQ-Rusty@users.noreply.github.com>
2022-02-09 22:21:12 -0500

    Update main.py
     - Added Rebuild Option to Main Menu and QuickPAK Function for Rapid Development

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from src.colors import colors as c
 from src.app_config import _version, _developer, _github_url, _app_name
 
 
+_last_pak_name = None
+_last_pak_folder_path = None
+
+
 def do_check_for_admin():
     if adminCheck():
         main_menu()
@@ -14,6 +18,7 @@ def do_check_for_admin():
 
 
 def main_menu():
+    global _last_pak_name, _last_pak_folder_path
     x = True
     while x:
         cls()
@@ -24,11 +29,14 @@ def main_menu():
         print(f"[{c.GREEN}2{c.END}] {c.LIGHT_GREEN}Extract PAKFile{c.END}")
         print(f"[{c.GREEN}3{c.END}] {c.LIGHT_GREEN}Build PAKFile{c.END}")
         print(f"[{c.GREEN}4{c.END}] {c.LIGHT_GREEN}Exit Application{c.END}")
+        if _last_pak_name is not None:
+            print(f"[{c.BLUE}5{c.END}] {c.LIGHT_BLUE}Rebuild Last PAKFile{c.END}")
+            print(f"[{c.RED}6{c.END}] {c.LIGHT_RED}Clear Rebuild Cache{c.END}")
         selection = get_input("\nChoice: " + c.LIGHT_PURPLE)
         clearColor()
         if selection.isdigit():
             selection = int(selection)
-            if 0 < selection <= 4:
+            if 0 < selection <= 6:
                 if selection == 1:
                     examine_pakfile()
                 elif selection == 2:
@@ -37,6 +45,13 @@ def main_menu():
                     create_pakfile()
                 elif selection == 4:
                     qExit()
+                elif _last_pak_name is not None and selection == 5:
+                    quick_create_pakfile(_last_pak_name, _last_pak_folder_path)
+                elif _last_pak_name is not None and selection == 6:
+                    _last_pak_name = None
+                    _last_pak_folder_path = None
+                else:
+                    raise MenuSelectionError(f"Invalid Menu Selection: Out of Bounds [1-4] ({selection})")
             else:
                 raise MenuSelectionError(f"Invalid Menu Selection: Out of Bounds [1-4] ({selection})")
         else:
@@ -61,9 +76,22 @@ def examine_pakfile():
 
 
 def create_pakfile():
+    global _last_pak_name, _last_pak_folder_path
     folder = get_input("Enter Folder Path: " + c.LIGHT_RED)
     pakname = get_input(c.END + "Enter PAKFile Path and Name: " + c.LIGHT_RED)
     clearColor()
+    print(f"Size of '{folder}': {c.BOLD + c.CYAN + get_folder_size(folder) + c.END}\n")
+    pause()
+    write_to_pakfile(folder, pakname)
+    print(f"Size of '{pakname}': {c.BOLD + c.CYAN + get_size_of_file(pakname) + c.END}\n")
+    validity_check(pakname)
+    print("All Files PAK'ed Successfully!\n")
+    _last_pak_name = pakname
+    _last_pak_folder_path = folder
+    pause()
+
+
+def quick_create_pakfile(pakname, folder):
     print(f"Size of '{folder}': {c.BOLD + c.CYAN + get_folder_size(folder) + c.END}\n")
     pause()
     write_to_pakfile(folder, pakname)
